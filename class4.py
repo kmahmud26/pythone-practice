7006356bebe5d8b40beb4c7d22a9463ba3e00b0f
khalid mahmud <khalidmahmud58@yahoo.com>
2023-06-10 05:00:22 -0700

    Pythone Class 4 Loop
    For Loop, White Loop, Prime Number, Odd and Even Number app

---
+++ class4.py
@@ -0,0 +1,94 @@
+# while loop
+       
+# z=1
+# while z < 10:
+#     print(z)
+#     z += 1
+
+# z=1
+# while z < 10:
+#     print(z)
+#     if z==6:
+#      break
+#     z += 1
+
+# z=1
+# while z < 10:
+#     z += 1
+#     if z == 4:
+#      continue
+#     print(z)
+
+# z=1
+# while z < 10:
+#     print(z)
+#     z += 1
+# else:
+#     print("i is no longer less than 10")
+  
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# numbers = [12, 14, 16]
+# for p in numbers:
+#     print(p)
+
+# numbers = [12, 14, 16]
+# for p in numbers:
+#     if p==14:
+#      break
+#     print(p)
+
+# numbers = [12, 14, 16]
+# for p in numbers:
+#     print(p)
+#     if p==14:
+#      break
+    
+# for o in range(2,20,4):
+#     print(o)
+# for o in range(2,20,4):
+#       if o == 16:
+#        break
+#       print(o)
+# for o in range(2,20,4):
+#     print(o)
+# for o in range(2,20,4):
+#        print(o)
+#        if o == 8:
+#         break
+# # even and odd number selection
+# N = int(input("enter your total element="))
+# p = []
+# k = []
+# m = []
+# for i in range(N):
+#     num = int(input("input your number ="))
+#     p.append(num)
+# print("My-List", p)
+# for i in p:
+#      if i%2==0:
+#       k.append(i)
+#      else:
+#       m.append(i)
+# print("Even List", k )
+# print("Odd List", m )
+# prime number
+# for z in range(1,101):
+#     count=0
+#     for p in range(1,z+1):
+#         if(z%p==0):
+#             count=count+1        
+#     if(count==2):
+#         print(z)
+    
+
+    
+      
+
+
+
+
+
+
